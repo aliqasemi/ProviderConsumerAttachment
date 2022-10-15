0c50939ff3bad2ef9d982a8653161c1891a6b57c
aliqasemi <qasemi4245qq@gmail.com>
2022-10-15 19:56:27 +0330

    add user register validation

--- signService/go.mod
+++ signService/go.mod
@@ -3,6 +3,7 @@ module github.com/aliqasemi/ProviderConsumerAttachment/signService
 go 1.18
 
 require (
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.9.1
@@ -11,6 +12,8 @@ require (
 )
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -22,6 +25,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -30,4 +34,5 @@ require (
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ signService/internal/handlers/http/validations/user/register.go
@@ -0,0 +1,58 @@
+package validation
+
+import (
+	"github.com/aliqasemi/ProviderConsumerAttachment/signService/internal/core/entities"
+	"github.com/go-playground/validator"
+)
+
+type (
+	UserInput struct {
+		FirstName       string `json:"first-name" form:"first-name" query:"first-name" param:"first-name" bson:"first-name,omitempty" validate:"required"`
+		LastName        string `json:"last-name" form:"last-name" query:"last-name" param:"last-name" bson:"last-name,omitempty" validate:"required"`
+		Password        string `json:"password" form:"password" query:"password" param:"password" bson:"password,omitempty" validate:"min=6,eqfield=ConfirmPassword"`
+		ConfirmPassword string `json:"confirm-password" form:"confirm-password" query:"confirm-password" param:"confirm-password" bson:"confirm-password,omitempty"`
+		PhoneNumber     uint   `json:"phone-number" form:"phone-number" query:"phone-number" param:"phone-number" bson:"phone-number,omitempty" validate:"min=10"`
+		Role            string `json:"role" form:"role" query:"role" param:"role" bson:"role,omitempty" validate:""`
+		Email           string `json:"email" form:"email" query:"email" param:"email" bson:"email,omitempty" validate:"required,email"`
+	}
+	RegisterValidator struct {
+		validator *validator.Validate
+	}
+	UserValidation interface {
+		validate(input UserInput) (bool, error)
+		buildModel(input UserInput) (entities.User, error)
+	}
+)
+
+func (input *UserInput) ValidateAndBuildEntity() (entities.User, error) {
+	validatorInput := &RegisterValidator{validator: validator.New()}
+	validate, err := validatorInput.validate(input)
+	if validate {
+		return validatorInput.buildEntity(input), nil
+	} else {
+		return entities.User{}, err.(validator.ValidationErrors)
+	}
+}
+
+func (validator *RegisterValidator) validate(input *UserInput) (bool, error) {
+	if err := validator.validator.Struct(input); err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+func (validator *RegisterValidator) buildEntity(input *UserInput) entities.User {
+	entity := entities.User{
+		Firstname:   input.FirstName,
+		Lastname:    input.LastName,
+		Email:       input.Email,
+		PhoneNumber: input.PhoneNumber,
+	}
+	if input.Role == "" {
+		entity.Role = "user"
+	} else {
+		entity.Role = input.Role
+	}
+	return entity
+}
