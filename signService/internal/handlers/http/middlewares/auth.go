358b695f056f6925095d76df09b3616bc6b2dd17
aliqasemi <qasemi4245qq@gmail.com>
2022-10-16 14:21:35 +0330

    add authenticate and authorize policy

--- signService/internal/handlers/http/middlewares/auth.go
+++ signService/internal/handlers/http/middlewares/auth.go
@@ -1,1 +1,39 @@
-package middlewares
+package middleware
+
+import (
+	"github.com/aliqasemi/ProviderConsumerAttachment/signService/internal/core/services"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func AuthApi(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authContext := &services.AuthContext{Context: c}
+		return next(authContext)
+	}
+}
+
+func ValidateToken() echo.MiddlewareFunc {
+	jwtConfig := middleware.JWTConfig{
+		SigningKey: services.JwtKey,
+		Claims:     &services.JWTClaim{},
+	}
+	return middleware.JWTWithConfig(jwtConfig)
+}
+
+func Authorize(role string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			authUser := services.AuthBuilder(c)
+			if authUser.HasRole(role) {
+				return next(c)
+			} else {
+				return &echo.HTTPError{
+					Message: "Unauthorized",
+					Code:    http.StatusForbidden,
+				}
+			}
+		}
+	}
+}
--- signService/internal/handlers/routes/router.go
+++ signService/internal/handlers/routes/router.go
@@ -2,12 +2,16 @@ package routes
 
 import (
 	"github.com/aliqasemi/ProviderConsumerAttachment/signService/internal/handlers/http/controllers"
+	middleware "github.com/aliqasemi/ProviderConsumerAttachment/signService/internal/handlers/http/middlewares"
 	"github.com/labstack/echo/v4"
 )
 
 func SetRoutes(e *echo.Echo) error {
-	e.GET("/users", controllers.Index)
 	e.POST("/register", controllers.Register)
 	e.POST("/login", controllers.Login)
+
+	authApi := e.Group("", middleware.AuthApi)
+	authApi = authApi.Group("", middleware.ValidateToken())
+	authApi.GET("/users", controllers.Index, middleware.Authorize("user"))
 	return nil
 }
